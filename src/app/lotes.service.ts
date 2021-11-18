dd837b4193da6e116c9df00889201da00e7b2296
DESKTOP-9TQ9S14\ramir <ramiropuccio@gmail.com>
2021-11-18 08:47:17 -0300

    css and indent changes

--- src/app/lotes.service.ts
+++ src/app/lotes.service.ts
@@ -8,22 +8,22 @@ export class LotesService {
   list() {
     return [
       {
-          "id": 1,
-          "name": "A1",
-          "farm_name": "Los cuervos",
-          "crop": "Maiz",
-          "water_per_week": "10.100",
-          "rains": ["3","1.1", "0.7"],
-          "irrigations": []
+        "id": 1,
+        "name": "A1",
+        "farm_name": "Los cuervos",
+        "crop": "Maiz",
+        "water_per_week": "10.100",
+        "rains": ["3","1.1", "0.7"],
+        "irrigations": []
       },
       {
-          "id": 2,
-          "name": "A2",
-          "farm_name": "Los cuervos",
-          "crop": "Trigo",
-          "water_per_week": "5.100",
-          "rains": ["0.5","0.3","1.2"],
-          "irrigations": []
+        "id": 2,
+        "name": "A2",
+        "farm_name": "Los cuervos",
+        "crop": "Trigo",
+        "water_per_week": "5.100",
+        "rains": ["0.5","0.3","1.2"],
+        "irrigations": []
       },
       {
         "id": 3,
@@ -33,9 +33,9 @@ export class LotesService {
         "water_per_week": "7.300",
         "rains": ["0.1","1.3","2.2"],
         "irrigations": []
-    }
-  ]
+      }
+    ]
   }
-  }
+}
   
 
--- src/app/lotes/lotes.component.ts
+++ src/app/lotes/lotes.component.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { LotesService } from 'src/app/lotes.service';
 @Component({
@@ -7,19 +6,21 @@ import { LotesService } from 'src/app/lotes.service';
   styleUrls: ['./lotes.component.scss']
 })
 export class LotesComponent{
-plots = [];
-currentPlot : 
-{"id":number,
-"name":string,
-"farm_name": string,
-"crop": string,
-"water_per_week": string,
-"rains",
-"irrigations", };
+  plots = [];
 
-precipitaciones:number;
-riegoSugerido;
-inputWater = 0 ;
+  currentPlot :  {
+    "id":number,
+    "name":string,
+    "farm_name": string,
+    "crop": string,
+    "water_per_week": string,
+    "rains",
+    "irrigations"
+  };
+
+  precipitaciones:number;
+  riegoSugerido;
+  inputWater = 0 ;
   constructor(
     private lotesService: LotesService,
   ) { }
@@ -27,16 +28,16 @@ inputWater = 0 ;
   ngOnInit() {
     this.plots = this.lotesService.list()   
   }
+
   selectPlot(id){
     this.currentPlot = this.plots[id-1]; 
     this.precipitaciones = (this.currentPlot.rains.reduce((a, b) => parseFloat(a) + parseFloat(b), 0)).toFixed(2);
   }
-  
+
   userInputWater(value, currentPlot){
     this.inputWater = value;
-   this.riegoSugerido = (parseFloat(currentPlot.water_per_week)-this.precipitaciones-this.inputWater) > 0 ? (parseFloat(currentPlot.water_per_week)-this.precipitaciones-this.inputWater).toFixed(2) : 0;
+    this.riegoSugerido = (parseFloat(currentPlot.water_per_week)-this.precipitaciones-this.inputWater) > 0 ? (parseFloat(currentPlot.water_per_week)-this.precipitaciones-this.inputWater).toFixed(2) : 0;
     this.currentPlot.irrigations = this.inputWater; 
-    
   }
-  
+
 }
